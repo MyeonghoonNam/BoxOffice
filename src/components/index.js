78a78acae5beba71d1d8f168af22c601cfd206a0
MyeongHoonNam <codenmh0822@naver.com>
2022-09-07 22:00:53 +0900

    refactor #1: Base, Domain 단위로 컴포넌트 역할 분리

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as SkipNavigation } from './SkipNavigation/index.js';
-export { default as ScreenReader } from './ScreenReader/index.js';
-export { default as Typography } from './Typography/index.js';
-export { default as Footer } from './Footer/index.js';
+export { default as ScreenReader } from './Base/ScreenReader/index.js';
+export { default as Typography } from './Base/Typography/index.js';
+export { default as Footer } from './Domain/Footer/index.js';
