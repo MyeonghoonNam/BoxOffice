1c36811d3fddecde8f8153b0fa9fb717588eb7ed
MyeongHoonNam <codenmh0822@naver.com>
2022-09-07 23:40:19 +0900

    feat #1: MovieListContainer 컴포넌트 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as SkipNavigation } from './SkipNavigation/index.js';
 export { default as ScreenReader } from './Base/ScreenReader/index.js';
 export { default as Typography } from './Base/Typography/index.js';
+export { default as SkipNavigation } from './Domain/SkipNavigation/index.js';
+export { default as MovieListContainer } from './Domain/MovieListContainer/index.js';
 export { default as Footer } from './Domain/Footer/index.js';
