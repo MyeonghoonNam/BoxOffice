ca2cf0778ec5074869ece931faaf640d4b7d4a3c
MyeongHoonNam <codenmh0822@naver.com>
2022-09-07 21:16:36 +0900

    feat #1: ScreenReader 컴포넌트 구현

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 export { default as SkipNavigation } from './SkipNavigation/index.js';
+export { default as ScreenReader } from './ScreenReader/index.js';
 export { default as Footer } from './Footer/index.js';
