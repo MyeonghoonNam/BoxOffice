b358e585a70839605dd47e8fc35cbc0c595fbdc8
MyeongHoonNam <codenmh0822@naver.com>
2022-09-07 23:41:03 +0900

    feat #1: App 컨트롤러 구현 및 적용

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import App from './App.js';
+
+const config = {
+	root: '#root',
+};
+
+const app = App();
+
+const render = () => {
+	window.requestAnimationFrame(() => {
+		const root = document.querySelector(config.root);
+		root.replaceWith(app(root));
+	});
+};
+
+render();
