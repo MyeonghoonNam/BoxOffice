7543ac0893f2c1f960f34c19556506abb28a22e9
남명훈 <57757719+MyeonghoonNam@users.noreply.github.com>
2022-09-13 13:03:33 +0900

    [Feature #33] MovieListContainer 컴포넌트 변환
    * feat #33: MovieListContainer 컴포넌트 변환 (#40)

--- src/components/Domain/MovieListContainer/index.tsx
+++ src/components/Domain/MovieListContainer/index.tsx
@@ -2,47 +2,19 @@ import { Typography, ScreenReader } from '../..';
 import MovieListHeader from './MovieListHeader';
 import MovieList from './MovieList';
 
-const COMPONENTS = {
-	Typography: Typography(),
-	ScreenReader: ScreenReader(),
-	MovieListHeader: MovieListHeader(),
-	MovieList: MovieList(),
-};
-
-const MovieListContainer = () => {
-	let $element;
-
-	const render = () => {
-		const $movieListContainer = document.createElement('section');
-		$movieListContainer.setAttribute('class', 'container-movie');
-
-		const $screenReader = COMPONENTS.ScreenReader({
-			tag: 'h1',
-			text: 'Hoon 시네마',
-		});
-
-		const $movieListTitle = COMPONENTS.Typography({
-			tag: 'h2',
-			attr: { class: 'container-title', value: 'heading' },
-			text: 'Hoon 시네마 영화 목록',
-		});
-
-		const $movieListHeader = COMPONENTS.MovieListHeader();
-		const $movieList = COMPONENTS.MovieList();
-
-		$movieListContainer.appendChild($screenReader);
-		$movieListContainer.appendChild($movieListTitle);
-		$movieListContainer.appendChild($movieListHeader);
-		$movieListContainer.appendChild($movieList);
-
-		return $movieListContainer;
-	};
-
-	return () => {
-		$element = render();
-
-		return $element;
-	};
-};
+function MovieListContainer() {
+	return (
+		<section className="container-movie">
+			<ScreenReader tag="h1" text="Hoon 시네마" />
+			<Typography
+				tag="h2"
+				attr={{ className: 'container-title', value: 'heading' }}
+				text="Hoon 시네마 영화 목록"
+			/>
+			<MovieListHeader />
+			<MovieList />
+		</section>
+	);
+}
 
 export default MovieListContainer;
