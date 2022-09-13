325dd614a5263da3099398ae91816cb50d3d4dc5
MyeongHoonNam <codenmh0822@naver.com>
2022-09-13 13:15:00 +0900

    remove #38: 불필요 주석 제거

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,3 @@
-// import App from './App.js';
-
-// const config = {
-// 	root: '#root',
-// };
-
-// const app = App();
-
-// const render = () => {
-// 	window.requestAnimationFrame(() => {
-// 		const root = document.querySelector(config.root);
-
-// 		if (root) {
-// 			root.replaceWith(app(root));
-// 		}
-// 	});
-// };
-
-// render();
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
