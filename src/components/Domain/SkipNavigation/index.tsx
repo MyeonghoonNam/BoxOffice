2941cd365415aa7acc14b7187a08ee5e6b799186
MyeongHoonNam <codenmh0822@naver.com>
2022-09-13 10:30:16 +0900

    feat #29: SkipNavigation 컴포넌트 변환

--- src/components/Domain/SkipNavigation/index.tsx
+++ src/components/Domain/SkipNavigation/index.tsx
@@ -1,22 +1,10 @@
-const SkipNavigation = () => {
-	let $element;
-
-	const render = () => {
-		const $skipNavigation = document.createElement('div');
-		$skipNavigation.setAttribute('class', 'nav-skip');
-		$skipNavigation.innerHTML = `
-      <a href="#nav-key">영화목록 바로가기</a>
+function SkipNavigation() {
+	return (
+		<div className="nav-skip">
+			<a href="#nav-key">영화목록 바로가기</a>
 			<a href="#company-key">회사정보 바로가기</a>
-    `;
-
-		return $skipNavigation;
-	};
-
-	return () => {
-		$element = render();
-
-		return $element;
-	};
-};
+		</div>
+	);
+}
 
 export default SkipNavigation;
